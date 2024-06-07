496246059c7f8cb12a2d4b5c97f9ce8316920c2a
Mauro Grageda <mauro.grageda@uap.edu.ar>
2024-06-07 11:27:48 -0300

    Tareas 1 y 2 completas. La tarea 3 me genera dudas de cuál sería en si la idea...

--- ClaseHilos/solution.cs
+++ ClaseHilos/solution.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Runtime.InteropServices;
+
 namespace ClaseHilos
 {
    internal class Producto
@@ -26,23 +29,68 @@ namespace ClaseHilos
         };
 
       static int precio_dolar = 500;
-
+      static readonly object locker = new object();
       static void Tarea1()
       {
-         throw new NotImplementedException();
-      }
+            lock (locker)
+            {
+                foreach (Producto producto in productos)
+                {
+                    //Console.WriteLine(producto.Nombre + "se aumento en 10 unidades la cantidad de stcok");
+                    producto.CantidadEnStock += 10;
+                    Console.WriteLine($"Stock actualizado: {producto.Nombre} - Nueva cantidad en stock: {producto.CantidadEnStock}");
+
+                }
+            }
+            //throw new NotImplementedException();
+        }
       static void Tarea2()
       {
-         throw new NotImplementedException();
+            lock (locker)
+            {
+
+
+                precio_dolar = 876;
+                    //producto.PrecioUnitarioDolares *= precio_dolar;
+                    //Console.WriteLine($"Precio actualizado: {producto.Nombre} - : ${producto.PrecioUnitarioDolares}");
+                
+            }
+         //throw new NotImplementedException();
       }
       static void Tarea3()
       {
-         throw new NotImplementedException();
+            //decimal precio_total = 0;
+            lock (locker)
+            {
+                foreach (Producto producto in productos)
+                {
+                    Console.WriteLine($"Stock actualizado: {producto.Nombre} - Nueva cantidad en stock: {producto.CantidadEnStock} - precio actualizado local : {producto.PrecioUnitarioDolares}");
+                    //precio_total += producto.PrecioUnitarioDolares;
+                    //Console.WriteLine($"Precio actualizado: {producto.Nombre} - precio actualizado : {producto.PrecioUnitarioDolares}");
+                }
+            }
+            //}//Console.WriteLine($"Precio total : {precio_total}");
+         //throw new NotImplementedException();
+
       }
 
       internal static void Excecute()
       {
-         throw new NotImplementedException();
+            Thread task1 = new Thread(new ThreadStart(Tarea1));
+            Thread task2 = new Thread(new ThreadStart(Tarea2));
+            Thread task3 = new Thread(new ThreadStart(Tarea3));
+
+            task1.Start();
+            task2.Start();
+            task3.Start();
+            
+            task1.Join();
+            task2.Join();
+            task3.Join();
+
+
+            //Console.WriteLine(productos);
+         //throw new NotImplementedException();
       }
    }
 }
